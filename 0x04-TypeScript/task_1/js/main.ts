d982b41a4d190319eff4097c3f2336dd6bf9c694
AjwadG <ajwadelgandouz@yahoo.com>
2024-05-01 21:23:13 +0200

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,17 +5,53 @@ interface Teacher {
   yearsOfExperience?: number,
   location: string,
   [propName: string]: any,
-}
+};
 
 interface Directors extends Teacher {
-  numberOfReports : number
-}
+  numberOfReports: number;
+};
 
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
-}
+};
 
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  return `${firstName[0]}. ${lastName}`
+  return `${firstName[0]}. ${lastName}`;
+};
+
+interface StudentClassInerFace {
+  displayName(): string
+  workOnHomework(): string
 }
-console.log(printTeacher('Ajwad', 'Elgandouz'));
+
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClassInerFace;
+}
+
+
+class StudentClass implements StudentClassInerFace, StudentClassInerFace {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  };
+
+  displayName(): string {
+    return this.firstName;
+  };
+
+  workOnHomework(): string {
+    return 'Currently working';
+  };
+
+}
+
+
+const student: StudentClass = new StudentClass('Ajwad', 'Elgandouz');
+
+console.log(student.lastName);
+console.log(student.firstName);
+console.log(student.displayName());
+console.log(student.workOnHomework());
